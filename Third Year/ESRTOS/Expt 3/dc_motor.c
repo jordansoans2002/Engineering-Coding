db644b7bad29eb8bcee9a0532c6f5f496c7dd430
jordansoans2002 <89215008+jordansoans2002@users.noreply.github.com>
2023-02-13 14:38:19 +0530

    Update dc_motor.c

--- Third Year/ESRTOS/Expt 3/dc_motor.c
+++ Third Year/ESRTOS/Expt 3/dc_motor.c
@@ -9,20 +9,21 @@ sbit sw = P3^7;
 
 sbit in11 = P2^0;
 sbit in12 = P2^1;
+sbit en1 = P2^2;
 
+sbit en2 = P2^5;
 sbit in21 = P2^6;
 sbit in22 = P2^7;
 void delay(unsigned int d){
 	unsigned int i,j;
 	for(i=0;i<d;i++)
-		for(j=0;j<1275;j++);
+		for(j=0;j<637;j++);
 }
 
 
 void turn_motor(unsigned int val){
 	while(1){
 		if(sw==0){ // if switch is pressed
-		
 			//turn motor clockwise
 			in11 = 1; 
 			in12 = 0;
@@ -35,32 +36,15 @@ void turn_motor(unsigned int val){
 			in21 = 0;
 			in22 = 1;
 		}
-	delay(val);
+		en1 = 1;
+		en2 = 1;
+		delay(val);
+		en1 = 0;
+		en2 = 0;
+		delay(val);
 	}
 }
 
 void main(){
-	init();
-	
-	P1 = 0xFF;
-	P3 = 0x00;
-	
-	intr = 1;
-	rd = 1;
-	wr = 1;
-	
-	while(1){
-		delay(10);
-		wr = 1;
-		delay(10); //high to low pulse on wr to start conversion
-		wr = 0;
-		while(intr == 1); // wait for conversion to finish
-		rd = 0; //rd low to read data
-		value = P1;
-		cmd(0x01);
-		turn_motor(value);
-		delay(10);
-		rd = 1;
-	}
-		
+	turn_motor(10);		
 }
