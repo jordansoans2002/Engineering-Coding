2489db70cba03572f88e62f302575fc6c6f727f4
jordansoans2002 <89215008+jordansoans2002@users.noreply.github.com>
2023-02-14 13:31:51 +0530

    Update dc_motor.c

--- Third Year/ESRTOS/Expt 3/dc_motor.c
+++ Third Year/ESRTOS/Expt 3/dc_motor.c
@@ -1,9 +1,4 @@
-#include <timer.h>
 #include <reg51.h>
-
-sbit wr = P3^0;
-sbit rd = P3^1;
-sbit intr = P3^2;
 
 sbit sw = P3^7;
 
@@ -14,14 +9,15 @@ sbit en1 = P2^2;
 sbit en2 = P2^5;
 sbit in21 = P2^6;
 sbit in22 = P2^7;
+
+//delay in multiples of 1ms
 void delay(unsigned int d){
 	unsigned int i,j;
 	for(i=0;i<d;i++)
 		for(j=0;j<637;j++);
 }
 
-
-void turn_motor(unsigned int val){
+void main(){
 	while(1){
 		if(sw==0){ // if switch is pressed
 			//turn motor clockwise
@@ -36,15 +32,12 @@ void turn_motor(unsigned int val){
 			in21 = 0;
 			in22 = 1;
 		}
+		//PWM given at enable pins
 		en1 = 1;
 		en2 = 1;
-		delay(val);
+		delay(10); //on for 10ms
 		en1 = 0;
 		en2 = 0;
-		delay(val);
+		delay(5); //off for 5ms
 	}
-}
-
-void main(){
-	turn_motor(10);		
 }
