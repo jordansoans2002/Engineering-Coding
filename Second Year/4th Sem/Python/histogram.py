deb5d5d1828420963faae58cd67b2e82487448c2
jordansoans2002 <89215008+jordansoans2002@users.noreply.github.com>
2023-02-24 13:47:44 +0530

    Delete Second Year directory

--- Second Year/4th Sem/Python/histogram.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-np.random.seed(1)
-data = np.random.randint(1, 100, 50)
-plt.hist(data, rwidth=0.9)  # plot the freq (no of repetitions) of the data
-plt.show()
-print(data)
