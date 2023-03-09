c69188bed7fe66f55bdfaf88d6e9e1b88eef9134
jordansoans2002 <89215008+jordansoans2002@users.noreply.github.com>
2023-03-09 12:42:28 +0530

    Update rtc_interface.ino

--- RTC/rtc_interface/rtc_interface.ino
+++ RTC/rtc_interface/rtc_interface.ino
@@ -7,45 +7,24 @@ DS1307RTC rtc;
 tmElements_t tm;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600); //set baud rate
   lcd.init();
 
+//set the date and time
   tm.Year = CalendarYrToTm(2023);
   tm.Month = 3;
   tm.Day = 9;
   tm.Hour = 12;
   tm.Minute = 30;
   tm.Second = 0;
+//write date and time to RTC
   RTC.write(tm);
- 
-//  rtc.begin(); //init rtc
-//  //rtc.set(0,30,11,9,3,2023); //11:30:00 09-03-2023  
-//  rtc.set(); //11:30:00 09-03-2023  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  uint8_t sec,min,hour,day,month;
-  uint16_t year;
-
+//read date and time from RTC
   RTC.read(tm);
-  //rtc.get(&sec, &min, &hour, &day, &month, &year);
-  
-//serial output
-  Serial.print("nTime: ");
-  Serial.print(tm.Hour);
-  Serial.print(":");
-  Serial.print(tm.Minute);
-  Serial.print(":");
-  Serial.print(tm.Second);
-  Serial.print("nDate: ");
-  Serial.print(tm.Day);
-  Serial.print(".");
-  Serial.print(tm.Month);
-  Serial.print(".");
-  Serial.print(tmYearToCalendar(tm.Year));
 
+//write to LCD
   lcd.clear();
   lcd.setCursor(1,0);
   lcd.print("Date: ");
