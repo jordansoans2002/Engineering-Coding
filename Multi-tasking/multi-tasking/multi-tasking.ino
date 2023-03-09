53499ad8b5dfeff3bc9234004a5788fa910648e2
jordansoans2002 <89215008+jordansoans2002@users.noreply.github.com>
2023-03-09 13:04:02 +0530

    Add files via upload

---
+++ Multi-tasking/multi-tasking/multi-tasking.ino
@@ -0,0 +1,34 @@
+#include <Arduino_FreeRTOS.h>
+
+void setup() {
+  Serial.begin(9600);
+  
+  xTaskCreate(task1,"Task1",100,NULL,1,NULL);
+  xTaskCreate(task2,"Task2",100,NULL,1,NULL);
+
+  //vTaskStartScheduler();
+}
+
+void loop() {}
+
+void task(int n){
+  while(1){
+    Serial.println("Task "+n);
+    vTaskDelay(1000/portTICK_PERIOD_MS);
+  }
+}
+
+void task1(void *param){
+  (void) param;
+  while(1){
+    Serial.println("Task1");
+    vTaskDelay(1000/portTICK_PERIOD_MS);
+  }
+}
+void task2(void *param){
+  (void) param;
+  while(1){
+    Serial.println("Task1");
+    vTaskDelay(1000/portTICK_PERIOD_MS);
+  }
+}
