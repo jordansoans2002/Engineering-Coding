48c40c52f174f0104e7c3522cd1be156921ec6a3
jordansoans2002 <89215008+jordansoans2002@users.noreply.github.com>
2023-03-17 15:04:00 +0530

    Expt 4 tictactoe

---
+++ ticmin.pl
@@ -0,0 +1,68 @@
+% Define the board as a list of 9 variables, where X represents a move by player X, O represents a move by player O, and empty squares are unbound variables.
+initial_board([_,_,_,_,_,_,_,_,_]).
+
+% Define the winning patterns.
+winning_pattern([1,2,3]).
+winning_pattern([4,5,6]).
+winning_pattern([7,8,9]).
+winning_pattern([1,4,7]).
+winning_pattern([2,5,8]).
+winning_pattern([3,6,9]).
+winning_pattern([1,5,9]).
+winning_pattern([3,5,7]).
+
+% Define the maximum depth of the search tree.
+max_depth(9).
+
+% Define the minimax algorithm.
+minimax(Board, Player, Depth, Score, Move) :-
+    % Check if the game is over.
+    (game_over(Board, Winner), score(Winner, Player, Score), Move is 0, !);
+    % Check if the maximum depth has been reached.
+    (Depth =:= max_depth, score(0, Player, Score), Move is 0, !);
+    % Otherwise, generate all possible moves.
+    findall([X,Y], (nth1(X, Board, empty), move(Board, X, Player, Y)), Moves),
+    % Find the best move using minimax.
+    best_move(Moves, Player, Depth, Score, Move).
+
+% Define the score function.
+score(Winner, Player, Score) :-
+    (Winner = Player, Score is 1);
+    (Winner \= Player, Winner \= 0, Score is -1);
+    (Winner = 0, Score is 0).
+
+% Define the game_over function.
+game_over(Board, Winner) :-
+    (winning_pattern(Pattern), winner(Board, Pattern, Winner));
+    (\+ member(empty, Board), Winner is 0).
+
+% Define the winner function.
+winner(Board, Pattern, Winner) :-
+    nth1(A, Pattern, X), nth1(B, Pattern, Y), nth1(C, Pattern, Z),
+    nth1(A, Board, X), nth1(B, Board, Y), nth1(C, Board, Z),
+    ((X = O, Y = O, Z = O, Winner is O) ; (X = X, Y = X, Z = X, Winner is X)).
+
+% Define the move function.
+move(Board, X, Player, NewBoard) :-
+    copy_term(Board, NewBoard),
+    nth1(X, NewBoard, empty),
+    nth1(X, NewBoard, Player).
+
+% Define the best_move function.
+best_move([Move], Player, Depth, Score, Move) :-
+    next_player(Player, NextPlayer),
+    minimax(Move, NextPlayer, Depth+1, Score, _).
+best_move([Move1|Moves], Player, Depth, BestScore, BestMove) :-
+    next_player(Player, NextPlayer),
+    minimax(Move1, NextPlayer, Depth+1, Score1, _),
+    best_move(Moves, Player, Depth, Score2, Move2),
+    (Player = x, best_score_x(Score1, Score2, BestScore), !;
+     Player = o, best_score_o(Score1, Score2, BestScore), !),
+    (BestScore = Score1, BestMove is Move1;
+     BestScore = Score2, BestMove is Move2).
+
+% Define the next_player function.
+next_player(x, o).
+next_player(o, x).
+
+% Define the
