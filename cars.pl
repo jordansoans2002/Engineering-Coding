ec84588dee4c1ab7e74266140fc5f4c8edab78a3
jordansoans2002 <89215008+jordansoans2002@users.noreply.github.com>
2023-03-25 20:52:04 +0530

    Update cars.pl

--- cars.pl
+++ cars.pl
@@ -10,3 +10,21 @@ canbuy(X,Y):-like(X,Y),forsale(Y).
 
 willbuy(X):-canbuy(X,Y).
 whowillbuy(Y):-like(X,Y).
+
+/*
+Will Sita buy a car?
+willbuy(sita).
+true.
+
+Who will buy BMW?
+?- like(X,bmw).
+X = ram ;
+X = sohan.
+
+Which cars are there for cell?
+?- forsale(X).
+X = bmw ;
+X = m800 ;
+X = ford.
+
+*/
